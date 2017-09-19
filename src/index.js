f98b3a820b75b5c8e20d356347b779dff71a9c39
Tatsiana Bukzheleva <ledimersedes@yandex.ru>
2017-09-19 14:10:39 +0300

    save some answer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+  var arr1 = first.split('');
+  var arr2 = second.split('');
+  var k=0;
+  var array = [];
+  var arr = [];
+  var t = 0;
+  for(var i=arr2.length-1; i>=0;i--){
+  	var temp = k;
+  	var pop = arr2[i];
+   
+  	for(var j=arr1.length-1; j>=0; j--){
+  		var mult  = arr1[j]*pop;
+      arr[k]=mult;
+  		k++;
+  	};	
+    k=temp;
+    k++;
+  	};
+   
+	return arr.reverse().join(''); 
+  };
+  
+
