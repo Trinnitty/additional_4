687fa811d4e848cafe18e39c9e83a4ec3061a0f7
Tatsiana Bukzheleva <ledimersedes@yandex.ru>
2017-09-23 23:50:33 +0300

    100%

--- src/index.js
+++ src/index.js
@@ -1,24 +1,39 @@
 module.exports = function multiply(first, second) {
   var arr1 = first.split('');
   var arr2 = second.split('');
-  var k=0;
-  var array = [];
+  var len = arr1.length + arr2.length;
   var arr = [];
-  var t = 0;
+  for(var t = 0; t<=len; t++){
+    arr[t] = 0;
+  }
+  var k=0;
   for(var i=arr2.length-1; i>=0;i--){
   	var temp = k;
   	var pop = arr2[i];
-   
+
   	for(var j=arr1.length-1; j>=0; j--){
   		var mult  = arr1[j]*pop;
-      arr[k]=mult;
+
+      arr[k]=arr[k]+mult;
+      if(arr[k]>9){
+        var d1 = parseInt(arr[k]/10);
+        var d2 = arr[k] - d1*10;
+        arr[k] = d2;
+        arr[k+1] = arr[k+1] + d1;
+      };
   		k++;
-  	};	
-    k=temp;
-    k++;
+  	};		
+  	k = temp;
+  	k++;
   	};
-   
-	return arr.reverse().join(''); 
+  
+  for(var a = arr.length-1; a>=arr.length-1; a--){
+    if(arr[a] == 0){
+      arr.pop();
+    }
+  }
+
+    return arr.reverse().join('');
   };
   
 
